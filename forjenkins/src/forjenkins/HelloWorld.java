1a6b1313e3afa34b7b924d8f2f5f7cdda191861d
Siddharth.Chaudhary1 <Siddharth.Chaudhary1@IN2467597W1.ey.net>
2021-06-25 16:20:35 +0530

    Committing the change from eclipse again

--- forjenkins/src/forjenkins/HelloWorld.java
+++ forjenkins/src/forjenkins/HelloWorld.java
@@ -2,6 +2,6 @@ package forjenkins;
 
 public class HelloWorld {
 public static void main(String[] args) {
-	System.out.println("Jenkins said hello");
+	System.out.println("Jenkins said hello from git");
 }
 }
