8fd8caccfe5cb3d19f6d859a846b7abedc3c1a2d
Siddharth.Chaudhary1 <Siddharth.Chaudhary1@IN2467597W1.ey.net>
2021-06-25 16:25:55 +0530

    Committing to the feature not the actual branch

--- forjenkins/src/forjenkins/HelloWorld.java
+++ forjenkins/src/forjenkins/HelloWorld.java
@@ -2,6 +2,6 @@ package forjenkins;
 
 public class HelloWorld {
 public static void main(String[] args) {
-	System.out.println("Jenkins said hello from git");
+	System.out.println("Jenkins said hello from git to feature");
 }
 }
